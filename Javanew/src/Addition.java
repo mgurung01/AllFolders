1563b546a9e0a88937f706485daa15a34eb7e410
mgurung01 <111322126+mgurung01@users.noreply.github.com>
2022-08-16 22:12:24 -0500

    All in one folder

---
+++ Javanew/src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 100;
+		int y = 500;
+		int z = x+y;
+		System.out.print("The total sum is:"+z);
+	}
+
+}
